bae923dea96b8c4edc30e9b0c10c72c17b93b089
Albert Costas <acostasg@uoc.edu>
2017-04-30 00:36:06 +0200

    Fixed error to index message elasticsearch

--- ElasticSearchCluster/schema/createIndexAndMappings.sh
+++ ElasticSearchCluster/schema/createIndexAndMappings.sh
@@ -3,7 +3,7 @@
 HOST="http://0.0.0.0:17560"
 
 echo "$(tput setaf 2) Add index and mapping message_index$(tput sgr 0)"
-curl -XPUT ${HOST}'/messages_index/massage' -X POST -d @messages_index.json
+curl -XPUT ${HOST}'/message_index/message' -X POST -d @messages_index.json
 
 echo "$(tput setaf 2) Add index and mapping token_index$(tput sgr 0)"
 curl -XPUT ${HOST}'/token_index/token' -X POST -d @token_index.json
