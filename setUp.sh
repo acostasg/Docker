1da99c6989ba27c45b7cb4f42c8fd0b9a221c0f8
Albert Costas <acostasg@uoc.edu>
2017-05-15 01:30:46 +0200

    Change ip 0.0.0.0 to view in local red server

--- setUp.sh
+++ setUp.sh
@@ -8,10 +8,10 @@ fuser -k 17561/tcp
 echo "$(tput setaf 2)Add Compiled binary and Start api POPmessage in local port 8080$(tput sgr 0)"
 cd ./API/
 docker build -t popmessageapi:latest .
-docker run -p 127.0.0.1:8080:8080 --name POPmessage -d popmessageapi
+docker run -p 0.0.0.0:8080:8080 --name POPmessage -d popmessageapi
 
 echo "$(tput setaf 2) Start elasticsearch and kibana in port 17560/17561$(tput sgr 0)"
 cd ../ElasticSearchCluster/
 docker-compose up -d
 
-echo "$(tput setaf 2) Finish setUp succes!!!$(tput sgr 0)"
+echo "$(tput setaf 2) Finish setUp success!!!$(tput sgr 0)"
